bb1a58b7ec6ee3788de13339b2328eacd9563df0
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-03-01 23:40:44 -0300

    primeira parte
    Nessa parte finalizei a primeira parte do contador

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Contador extends React.Component {
+
+  constructor() {
+
+    super();
+    this.state = {
+
+      contador:0
+
+    }
+  }
+
+  
+ 
+  render(){
+    return (
+      <h1>{this.state.contador}</h1>
+      <div>
+        <button onClick={}>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
 }
-
-export default App;
